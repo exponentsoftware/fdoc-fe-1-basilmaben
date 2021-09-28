60b8a339200c214327508ef5ee72713dd8a702ab
basilmaben <basilmaben.bm@gmail.com>
2021-09-28 23:57:58 +0530

    state uplifting

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Album from "./Album";
 import styled from 'styled-components'
+import { Link } from "react-router-dom"
 
-const Albums = ({ albums }) => {
+const Albums = ({ data, filter }) => {
+  const [input, setInput] = useState("")
+  const searchHandler = e => {
+    const val = e.target.value
+    setInput(val)
+  }
   return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <input type="search" placeholder onChange={searchHandler} />
+        <button onClick={() => filter(input)}>Search</button>
+      </div>
     <Container>
-      {albums.map((album) => (
-        <Album album={album} /> 
+      {data.map(album => (
+          <Link to={`/albums/${album.id}`}><Album album={album} /></Link>
       ))}
     </Container>
+    </>
   );
 };
 
