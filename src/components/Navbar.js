a840b9840fac0e3383d1d1e22d7f9ba8ca75142c
basilmaben <basilmaben.bm@gmail.com>
2021-09-29 23:56:58 +0530

    omitted some styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,38 +4,21 @@ import {
   Nav,
   NavbarContainer,
   NavLogo,
-  MobileIcon,
-  NavMenu,
-  NavItem,
-  NavLinks,
   NavBtn,
   NavBtnLink,
   NavBtnLink2,
 } from './NavbarElements';
 
 const Navbar = () => {
+
   return (
     <>
       <Nav>
         <NavbarContainer>
-        <NavLogo to='/' ><img src={logo} alt="logo" style={{ height: "40px", width: "40px", margin: "0 10px 0 0" }}/>Spotify</NavLogo>
-          <MobileIcon>
-            <FaBars />
-          </MobileIcon>
-          <NavMenu>
-            <NavItem>
-              <NavLinks to='discover'>Premium</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to='album'>Album</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to='playlists'>Playlists</NavLinks>
-            </NavItem>          
-          </NavMenu>
+       <NavLogo to='/' ><img src={logo} alt="logo" style={{ height: "40px", width: "40px", margin: "0 10px 0 0" }}/>Spotify</NavLogo>
           <NavBtn>
-            <NavBtnLink2 to='signup'>Sign Up</NavBtnLink2> 
-            <NavBtnLink to='signin'>Sign In</NavBtnLink>
+            <NavBtnLink2 to='add'>Add Album</NavBtnLink2> 
+            <NavBtnLink to='albums'>Albums</NavBtnLink>
           </NavBtn>
         </NavbarContainer>
       </Nav>
